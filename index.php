46aea22c45b616c046112c40e1a828edb269b945
Carlos Lopez <>
2018-04-02 17:44:41 -0500

    Commit inicial / login y conexion a base de datos MySQL

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="estilo.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <p class="titulo">
+            LOGIN
+        </p>
+        <form action="procesador.php">
+            <input type="text" name="usuario"> <br>
+            <input type="password" name="contraseña"> <br>
+            <input type="submit" name="boton" value="ingresar"> <br>
+        </form>
+    </body>
+</html>
---
+++ procesador.php
@@ -0,0 +1,3 @@
+<?php
+mysqli_connect("localhost", "root", "mysql2018", "loginud", "3306");
+ echo "conected";
